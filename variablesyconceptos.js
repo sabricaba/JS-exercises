9697fa80e2e11660874174fb0874c95965a6e6b6
Sabrina Cabaña <sabricaba@gmail.com>
2020-10-26 22:07:23 -0300

    Ejercicio numero 1

---
+++ variablesyconceptos.js
@@ -0,0 +1,16 @@
+var numeroUno = 10;
+var numeroDos = 3;
+
+console.log(numeroUno + numeroDos);
+
+var stringUno = "Berta es mi perra.";
+var stringDos = "Ella es muy linda.";
+var stringTres = stringUno + " " + stringDos;
+
+console.log(stringTres);
+
+var miNombre = "Sabrina"
+var miApellido = "Cabana"
+var nombreApellido = miNombre.length + miApellido.length;
+
+console.log(nombreApellido);
